a1f1e06c68f6cda615a15b91290906150d32fd44
Dario Buitrago <da.buitragoc1@uniandes.edu.co>
2022-11-06 16:07:53 -0500

    :sparkles: Punto 3. prueba de componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'parcial-angular-tu-segundazo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('parcial-angular-tu-segundazo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('parcial-angular-tu-segundazo app is running!');
-  });
 });
--- src/app/car/car-list/car-list.component.spec.ts
+++ src/app/car/car-list/car-list.component.spec.ts
@@ -1,15 +1,24 @@
 /* tslint:disable:no-unused-variable */
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Car } from '../car';
+import { By } from '@angular/platform-browser';
+
 
 import { CarListComponent } from './car-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CarService } from '../car.service';
 
 describe('CarListComponent', () => {
   let component: CarListComponent;
   let fixture: ComponentFixture<CarListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CarListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CarListComponent ],
+      providers: [CarService]
     })
     .compileComponents();
   }));
@@ -17,10 +26,21 @@ describe('CarListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CarListComponent);
     component = fixture.componentInstance;
+
+    component.cars.push(new Car(1, "Reault", "Duster", "life", 2019, 12434, "rojo", "la imagen 1"))
+    component.cars.push(new Car(1, "Reault", "Sandero", "eco", 2009, 3431, "verde", "la imagen 2"))
+    component.cars.push(new Car(1, "Chevrolet", "Onix", "Steal", 2021, 4422, "azul", "la imagen 3"))
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('table must have 1 heade and 3 rows', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
+  });
+
 });
