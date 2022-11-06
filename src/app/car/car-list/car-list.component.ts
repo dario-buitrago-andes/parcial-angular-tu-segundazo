89141daed0c1d456b74050abc20f10d088c8f583
Dario Buitrago <da.buitragoc1@uniandes.edu.co>
2022-11-06 15:36:31 -0500

    :sparkles: Punto 2. conteo por marcas

--- src/app/car/car-list/car-list.component.ts
+++ src/app/car/car-list/car-list.component.ts
@@ -9,6 +9,7 @@ import { CarService } from '../car.service';
 })
 export class CarListComponent implements OnInit {
   cars: Array<Car> = [];
+  totals: Array<string> = []
 
   constructor(
     private carService: CarService,
@@ -17,7 +18,14 @@ export class CarListComponent implements OnInit {
   getCars() : void {
     this.carService.getCars().subscribe(cars => {
       this.cars = cars;
+      this.getTotals()
     })
+  }
+
+  getTotals() : void {
+    let carsMap = new Map();
+    this.cars.forEach(car => carsMap.set(car.marca, this.cars.filter(carfilter => carfilter.marca === car.marca).length))
+    carsMap.forEach((value, key) => this.totals.push("Total ".concat(key).concat(": ").concat(value)))
   }
 
   ngOnInit() {
